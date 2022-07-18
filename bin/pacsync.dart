196ef66db04a03fc750c456dff3832a01a71a83e
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-07-18 20:06:21 +0200

    create dart package

---
+++ bin/pacsync.dart
@@ -0,0 +1,5 @@
+import 'package:pacsync/pacsync.dart' as pacsync;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pacsync.calculate()}!');
+}
---
+++ test/pacsync_test.dart
@@ -0,0 +1,8 @@
+import 'package:pacsync/pacsync.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
