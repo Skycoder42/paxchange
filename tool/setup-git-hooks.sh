cce97048bb523f8b613c9a7fd75e31296787ea4b
Skycoder42 <Skycoder42@users.noreply.github.com>
2022-11-01 13:17:53 +0100

    fix deprecated api

--- tool/setup-git-hooks.sh
+++ tool/setup-git-hooks.sh
@@ -3,6 +3,6 @@ set -eo pipefail
 
 mkdir -p .git/hooks
 echo "#!/bin/sh" > .git/hooks/pre-commit
-echo "set -eo pipefail" >> .git/hooks/pre-commit
-echo "exec dart run dart_pre_commit --no-lib-exports --no-flutter-compat" >> .git/hooks/pre-commit
+echo "set -e" >> .git/hooks/pre-commit
+echo "exec dart run dart_pre_commit" >> .git/hooks/pre-commit
 chmod a+x .git/hooks/pre-commit
